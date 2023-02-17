ac2ec0d69a2dde1032bd6e41adc09b16b68bd3d8
javier cerda ibañez <javiercerdaibaez@gmail.com>
2023-02-17 17:37:55 +0100

    Marvel mejorada

--- src/app/characters.ts
+++ src/app/characters.ts
@@ -23,11 +23,6 @@ export interface Characters {
     modified: string
     thumbnail: Thumbnail
     resourceURI: string
-    comics: Comics
-    series: Series
-    stories: Stories
-    events: Events
-    urls: Url[]
   }
   
   export interface Thumbnail {
@@ -35,57 +30,6 @@ export interface Characters {
     extension: string
   }
   
-  export interface Comics {
-    available: number
-    collectionURI: string
-    items: Item[]
-    returned: number
-  }
   
-  export interface Item {
-    resourceURI: string
-    name: string
-  }
-  
-  export interface Series {
-    available: number
-    collectionURI: string
-    items: Item2[]
-    returned: number
-  }
-  
-  export interface Item2 {
-    resourceURI: string
-    name: string
-  }
-  
-  export interface Stories {
-    available: number
-    collectionURI: string
-    items: Item3[]
-    returned: number
-  }
-  
-  export interface Item3 {
-    resourceURI: string
-    name: string
-    type: string
-  }
-  
-  export interface Events {
-    available: number
-    collectionURI: string
-    items: Item4[]
-    returned: number
-  }
-  
-  export interface Item4 {
-    resourceURI: string
-    name: string
-  }
-  
-  export interface Url {
-    type: string
-    url: string
-  }
+ 
   
